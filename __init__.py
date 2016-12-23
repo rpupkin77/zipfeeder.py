d6c2a951b0def2502b226b1b36ac45e3a15fadff
Paul Thompson <pthompso@akamai.com>
2016-12-23 14:58:41 -0500

    first commit

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from app.connector import Connector as ZipFeeder
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'zipfeeder',
+  packages = ['zipfeeder'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Uses the Zipfeeder.us API to serve free United States Zip Code information, a ZipFeeder account '
+                'is required',
+  author = 'Paul Thompson',
+  author_email = 'info@zipfeeder.us',
+  url = 'https://github.com/peterldowp', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
